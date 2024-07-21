cc6219b46ebb9191edf5018d1e55e67233e4c753
Harshit <117671072+hyperalert@users.noreply.github.com>
2024-07-21 23:37:38 +0530

    Update RollingHash.cpp
    1. Added class for so that multiple mods can be used
    2. Less global variable
    3. Less use of long long

--- String/RollingHash.cpp
+++ String/RollingHash.cpp
@@ -1,62 +1,66 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-const int N=1e6+6;
-const int mod=1e9+7;
-const int base=31;
-int pw[N];
-int inv[N];
-int Hash[N];
-
-
 int add(int a,int b,int mod){
-    int res=(a+b)%mod;
-    if(res<0)
-        res+=mod;
-    return res;
+  int res=(a%mod+b%mod)%mod;
+  return res;
 }
 int mul(int a,int b,int mod){
-    int res=(a*1LL*b)%mod;
-    if(res<0)
-        res+=mod;
-    return res;
+  int res=(a*1LL*b)%mod;
+  return res;
 }
-int power(int a,int b,int mod){
-    int res=1;
-    while(b)
-    {
-        if(b&1)
-            res=mul(res,a,mod);
-        a=mul(a,a,mod);
-        b=b>>1;
-    }
-    return res;
+int pow(int a,int b,int mod){
+  int res=1;
+  while(b){
+    if(b&1)
+      res=mul(res,a,mod);
+    a=mul(a,a,mod);
+    b=b>>1;
+  }
+  return res;
 }
-void precalc(){
-    pw[0]=1;
-    for(int i=1;i<N;++i)
-        pw[i]=mul(pw[i-1],base,mod);
-    
-    int pw_inv=power(base,mod-2,mod);
-    inv[0]=1;
-    for(int i=1;i<N;++i)
-        inv[i]=mul(inv[i-1],pw_inv,mod);
+int p=33;
+void preCalcPow(vector<int>& pw,int mod){
+  int n=pw.size();
+  pw[0]=1;
+  for(int i=1;i<n;++i)
+    pw[i]=mul(pw[i-1],p,mod);
 }
-void build(string &s){
-    int n=s.length();
-    for(int i=0;i<n;++i){
-        Hash[i]=add((i==0)?0:Hash[i-1],mul(pw[i],s[i]-'a'+1,mod),mod);
-    }
+void preCalcInv(vector<int>& inv_pw,int mod){
+  int n=inv_pw.size();
+  int inv=pow(p,mod-2,mod);
+  inv_pw[0]=1;
+  for(int i=1;i<n;++i)
+    inv_pw[i]=mul(inv_pw[i-1],inv,mod);
 }
-int getHash(int x,int y){
-    int res=add(Hash[y],(x==0)?0 : -Hash[x-1],mod);
-    res=mul(res,(x==0)?1:inv[x],mod);
-    return res;
+void preCalcHash(vector<int>& hash,vector<int>& pow,string& s,int mod){
+  int n=hash.size();
+  for(int i=0;i<n;++i)
+    hash[i]=add((i==0?0:hash[i-1]),mul(s[i]-'a'+1,pow[i],mod),mod);
 }
-int main()
-{
- string s="abcabc";
- precalc();
- build(s);
- cout<<getHash(0,2)<<' '<<getHash(3,5)<<endl;
+
+class Hash{
+  public:
+   vector<int> pw,inv_pw,hash;
+   int mod;
+   string s;
+   Hash(string a,int m){
+     s=a;
+     mod=m;
+     int n=s.length();
+     pw.resize(n+1);
+     inv_pw.resize(n+1);
+     hash.resize(n+1);
+     preCalcPow(pw,mod);
+     preCalcInv(inv_pw,mod);
+     preCalcHash(hash,pw,s,mod);
+   }
+   int subString(int i,int j){
+     int res=add(hash[j],(i==0?0:-hash[i-1]),mod);
+     res=mul(res,inv_pw[i],mod);
+     return res;
+   }
+};
+int main(){
+  
 }
